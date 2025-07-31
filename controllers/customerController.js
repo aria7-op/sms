4ce490e7280d932607576b44065e752633936cee
aria7-op <ss.ariadelta@gmail.com>
2025-07-31 06:34:09 -0400

    Update customerController.js

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -37,9 +37,9 @@ async function initializeDbPool() {
       const dbConfig = {
         host: process.env.DB_HOST || 'localhost',
         port: process.env.DB_PORT || 3306,
-        user: process.env.DB_USER || 'mohammad1_ahmadi1',
-        password: process.env.DB_PASSWORD || 'mohammad112_',
-        database: process.env.DB_NAME || 'mohammad1_school',
+        user: process.env.DB_USER || 'school',
+        password: process.env.DB_PASSWORD || 'YourName123!',
+        database: process.env.DB_NAME || 'school',
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0,
