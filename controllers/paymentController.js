68a82ec5e0b3dd4d341f21b3d282839f6fe17c9f
aria7-op <ss.ariadelta@gmail.com>
2025-08-20 01:08:53 -0400

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -136,8 +136,6 @@ class PaymentController {
             })
           };
 
-          console.log('Prisma client:', prisma);
-          console.log('Prisma client type:', typeof prisma);
           const payment = await prisma.payment.create({
             data: createData,
             include: {
