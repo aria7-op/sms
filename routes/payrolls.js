f30dc90ff8981dc70c8b6ce532c0e165cab40fde
aria7-op <ss.ariadelta@gmail.com>
2025-08-28 00:34:49 -0400

    Update payrolls.js

--- routes/payrolls.js
+++ routes/payrolls.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import { getAllPayrolls, getPayrollById, createPayroll, updatePayroll, deletePayroll } from '../controllers/payrollController.js';
+import { authenticateToken, authorizeRoles } from '../middleware/auth.js';
+
 const router = express.Router();
+
+// Apply authentication to all routes
+router.use(authenticateToken);
 
 router.get('/', getAllPayrolls);
 router.get('/:id', getPayrollById);
-router.post('/', createPayroll);
-router.put('/:id', updatePayroll);
-router.delete('/:id', deletePayroll);
+router.post('/', authorizeRoles(['ACCOUNTANT', 'SCHOOL_ADMIN', 'TEACHER', 'SUPER_ADMIN']), createPayroll);
+router.put('/:id', authorizeRoles(['ACCOUNTANT', 'SCHOOL_ADMIN', 'TEACHER','SUPER_ADMIN']), updatePayroll);
+router.delete('/:id', authorizeRoles(['ACCOUNTANT', 'SCHOOL_ADMIN', 'TEACHER','SUPER_ADMIN']), deletePayroll);
 
 export default router; 
